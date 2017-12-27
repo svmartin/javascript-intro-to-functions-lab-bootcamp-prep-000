6612c54861517f349f71884c1b04b602a90e2423
Sean Martin <sean.martin@gmail.com>
2017-12-27 18:49:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (string.toUpperCase() === string) {
     return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
   }
 }
 // logShout('hello')
